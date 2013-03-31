55b6999460763f90234494904bb4640cc50f588b
Jason Carlisle Mann <jcm2207@columbia.edu>
2013-03-31 16:16:45 -0400

    keeping track of an old version for testing

--- isola.cpp
+++ isola.cpp
@@ -20,7 +20,7 @@
 #include <limits>		/* min and max for types */
 #include <ctime>		/* for system time */
 #include <sstream>		/* stringstream */
-
+#include <algorithm>
 
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
